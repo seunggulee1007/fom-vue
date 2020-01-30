f3c302665527ad2a94f38147bdd353973e95f2ba
kyuhcho <kyuhcho@smilegate.com>
2020-01-30 09:37:58 +0900

    Init project
    init project for FIM
    
    1. Using spring-boot
    2. Using Mybatis

---
+++ src/main/java/net/smilegate/FimWebApplication.java
@@ -0,0 +1,13 @@
+package net.smilegate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FimWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FimWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/smilegate/FimWebApplicationTests.java
@@ -0,0 +1,13 @@
+package net.smilegate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FimWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
