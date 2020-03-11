a65810f744bdc13d88f8a1496a5b996f565cd7a2
sglee <leesg107@naver.com>
2020-03-11 16:53:39 +0900

    주석 추가

--- src/main/java/net/smilegate/fim/mappers/fim/BizMapper.java
+++ src/main/java/net/smilegate/fim/mappers/fim/BizMapper.java
@@ -9,8 +9,18 @@ import net.smilegate.fim.vo.BizVO;
 @Mapper
 public interface BizMapper {
     
+    /**
+     * 사업자 휴폐업 조회 이력 저장
+     * @param bizVO
+     * @return
+     */
     public int insertBizInfo(BizVO bizVO);
     
+    /**
+     * 사업자 휴폐업 조회 이력 조회
+     * @param bizVO
+     * @return
+     */
     public List<BizVO> selectBizInfoList(BizVO bizVO);
     
 }
