ff14ed0a49a6851b6fc70219dfb072750f5b7e54
sglee <leesg107@naver.com>
2020-04-09 14:18:45 +0900

    테스트 푸시 (기존 브런치에 영향 가는지)

---
+++ src/main/webapp/resources/fim/js/AUIGrid/AUIGridLicense.js
@@ -0,0 +1,9 @@
+/*!
+ * AUIGrid 3.0.7 License
+ * License Type : Evaluation(Trial) Version
+ * Authorized Domain(or IP) : localhost,127.0.0.1
+ * Expiration Date : 2020-02-06
+ * www.auisoft.net
+ */
+ /* eslint-disable */
+AUIGridLicense = "eyJjdCI6InFjYXJNbXF6VGtSNW9MSWR2b0ZZQSs5aVlOVXAwOVNBN1l5VVwvd3oxVVQ1UmpPMDhoWTlQZFprNHBpaENJTWhyeEpTNk40b1ZPamdyWUZwMUxWRkNlRThJZXJKYXZBcWF4SDVpTHhuV1loZz0iLCJpdiI6IjFmNzk1MzE4YmI5NGNhNDA4OWM2MTc2MTY1NzlmODliIiwicyI6IjZlMjY3MDZlZDU4YTZjNDYifQ==";
